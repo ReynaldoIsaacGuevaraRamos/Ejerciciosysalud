4b0827a699cd734dc9d94a9a803ade70fd984090
Isaac Ramos <66816706+reynaldoisaacguevararamos@users.noreply.github.com>
2021-05-01 21:24:57 -0600

    Prueba de ramas

--- app/src/main/java/sv/ues/fia/ejerciciosysalud/ui/perfil/PerfilViewModel.kt
+++ app/src/main/java/sv/ues/fia/ejerciciosysalud/ui/perfil/PerfilViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class PerfilViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "@string/perfil_estatura"
     }
     val text: LiveData<String> = _text
 }
